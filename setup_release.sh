56ae716cef76203f6ffa748b2b696908d49f8c9b
Saad Khattak <saadrustam@gmail.com>
2018-03-25 18:36:23 -0400

    Added setup generation | Updated Travis script

--- setup_release.sh
+++ setup_release.sh
@@ -2,9 +2,9 @@
 # auto-generated file, do not modify
 # -----------------------------------------------------------------------------
 
-rm -rf build
-mkdir build
+rm -rf build_release
+mkdir build_release
 
-cd build
-cmake ../ -DCMAKE_BUILD_TYPE=Debug -DCMAKE_INSTALL_PREFIX=../install -DTLOC_DETAILED_LOGS=ON
+cd build_release
+cmake ../ -DCMAKE_BUILD_TYPE=Release -DCMAKE_BUILD_TYPE=Debug -DCMAKE_INSTALL_PREFIX=../install -DTLOC_DETAILED_LOGS=ON
 make install
